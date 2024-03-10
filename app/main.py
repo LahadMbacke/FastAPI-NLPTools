300d03f758867b9f77afd59c41599ba0f41e02c4
Lahad Mbacke <lahadmbacke00@gmail.com>
2024-03-10 22:27:34 +0100

    Javascript consume api ner

--- app/main.py
+++ app/main.py
@@ -1,11 +1,13 @@
-from fastapi import FastAPI
-from fastapi.responses import HTMLResponse  
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
 import ner
 
-
 app = FastAPI()
+templates = Jinja2Templates(directory="templates")
+@app.get("/ner", response_class=HTMLResponse)
+async def get_ner(request: Request, text: str = ""):
+    html_content = ner.ner_spacy(text)
+    return templates.TemplateResponse("ner.html", {"request": request, "html_content": html_content})
 
-@app.get("/ner")
-def get_ner(text: str):
-    return ner.ner_spacy(text)
-    
+# Votre fonction ner_spacy reste la même
