546b28d5fb797a77a0488c43801baec76e49b08c
Lahad Mbacke <lahadmbacke00@gmail.com>
2024-03-10 23:05:02 +0100

    this version works but hasn't css

--- app/main.py
+++ app/main.py
@@ -2,12 +2,20 @@ from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 import ner
+from pydantic import BaseModel
+
+
+
+class Text(BaseModel):
+    text: str
 
 app = FastAPI()
 templates = Jinja2Templates(directory="templates")
-@app.get("/ner", response_class=HTMLResponse)
-async def get_ner(request: Request, text: str = ""):
-    html_content = ner.ner_spacy(text)
-    return templates.TemplateResponse("ner.html", {"request": request, "html_content": html_content})
+@app.get("/", response_class=HTMLResponse)
+async def home(request: Request, text: str = ""):
+    return templates.TemplateResponse("ner.html", {"request": request})
 
-# Votre fonction ner_spacy reste la même
+
+@app.post("/ner")
+async def ner_api(text: Text):
+    return ner.ner_spacy(text.text)
