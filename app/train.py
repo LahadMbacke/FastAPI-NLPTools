b0c6db284295a1dea34917f30ed47dc3b3492fdc
Lahad Mbacke <lahadmbacke00@gmail.com>
2024-03-13 13:30:08 +0100

    Recupration des texts et Tags du Json pour creer des donnees de trainer et converti en numpy

--- app/train.py
+++ app/train.py
@@ -1,5 +1,6 @@
 import json
-from utils import tokeniser, stemmer_text
+import numpy as np
+from utils import tokeniser, stemmer_text, bag_of_words
 
 with open('../data/intents.json', 'r') as f:
     data = json.load(f)
@@ -17,3 +18,17 @@ for intent in data['intents']:
         word_tag_pairs.append((word, tag))
 
 
+all_words = [sorted(set(all_words))]
+tags = sorted(set(tags))
+
+X_train = []
+y_train = []
+
+for (pattern, tag) in word_tag_pairs:
+    bag = bag_of_words(pattern, all_words) # TODO: Implement bag_of_words function
+    X_train.append(bag) # X_train is a list of lists
+    label = tags.index(tag)
+    y_train.append(label) # y_train is a list of integers
+
+X_train = np.array(X_train)
+y_train = np.array(y_train)
